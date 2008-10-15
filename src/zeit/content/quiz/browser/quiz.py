99ee33ffd67ee6c57b1c8f0d573e6a292758ebc6
Michael Howitz <mh@gocept.com>
2008-10-15 12:13:39 +0000

    quiz views are now in one file

--- src/zeit/content/quiz/browser/quiz.py
+++ src/zeit/content/quiz/browser/quiz.py
@@ -4,8 +4,17 @@
 # $Id$
 
 import zope.cachedescriptors.property
+import zope.formlib.form
 
+import zeit.cms.browser.form
+import zeit.cms.content.browser.form
 import zeit.cms.content.interfaces
+import zeit.cms.interfaces
+
+from zeit.cms.i18n import MessageFactory as _
+
+import zeit.content.quiz.interfaces
+import zeit.content.quiz.quiz
 
 
 class Questions(object):
@@ -14,3 +23,32 @@ class Questions(object):
     def metadata(self):
         return zeit.cms.content.interfaces.ICommonMetadata(self.context)
 
+
+class QuizFormBase(object):
+
+    form_fields = zope.formlib.form.FormFields(
+        zeit.cms.interfaces.ICMSContent,
+        zeit.cms.syndication.interfaces.IAutomaticMetadataUpdate,
+        zeit.cms.content.interfaces.ICommonMetadata)
+
+
+class AddQuiz(QuizFormBase,
+              zeit.cms.content.browser.form.CommonMetadataAddForm):
+
+    title = _("Add quiz")
+    factory = zeit.content.quiz.quiz.Quiz
+    next_view = 'questions.html'
+    form_fields = QuizFormBase.form_fields.omit(
+        'automaticMetadataUpdateDisabled')
+
+
+class EditQuiz(QuizFormBase,
+               zeit.cms.content.browser.form.CommonMetadataEditForm):
+
+    title = _("Edit quiz")
+
+
+class DisplayQuiz(QuizFormBase,
+                  zeit.cms.content.browser.form.CommonMetadataDisplayForm):
+
+    title = _("View quiz metadata")
