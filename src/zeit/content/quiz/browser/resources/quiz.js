cd1d73e37b27034f2261f1b602f6e164bdd52d7c
Thomas Lotze <tl@gocept.com>
2008-10-23 12:06:11 +0000

    avoid clicking links that were used as drag handles

--- src/zeit/content/quiz/browser/resources/quiz.js
+++ src/zeit/content/quiz/browser/resources/quiz.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2008 gocept gmbh & co. kg
 // See also LICENSE.txt
 
-connect(window, 'onload', function() {
-    class_names = ['answers', 'questions']; // order is important
+MochiKit.Signal.connect(window, 'onload', function() {
+    var class_names = ['answers', 'questions']; // order is important
     forEach(class_names, function(class_name) {
         forEach(getElementsByTagAndClassName('ol', class_name),
                 function(list) {
@@ -10,5 +10,19 @@ connect(window, 'onload', function() {
                 list.id, {hoverclass: 'questionsort-hover',
             });
         });
+    });
+
+    MochiKit.Signal.connect(MochiKit.DragAndDrop.Draggables, 'end',
+                            function (event) {
+                                event.element.dragging = true;
+                            });
+
+    var questions = getFirstElementByTagAndClassName('ol', 'questions');
+    MochiKit.Signal.connect(questions, 'onclick', function(event) {
+        var item = event._event.target.parentNode
+        if (item.dragging) {
+            event.stop();
+            item.dragging = false;
+        };
     });
 });
