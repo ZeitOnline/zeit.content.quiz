d5b7000af35529b56387c4674b772f5ce0daab83
Christian Zagrodnick <cz@gocept.com>
2008-10-27 14:30:18 +0000

    preparing release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.quiz',
-    version='0.1.0dev',
+    version='0.1dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms/zeit.content.quiz',
