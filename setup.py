32e837950b3366bb13f6bf847e376b7dea84504d
Wolfgang Schnerring <ws@gocept.com>
2014-12-17 15:51:54 +0100

    Preparing release 0.5.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.quiz',
-    version='0.5.11.dev0',
+    version='0.5.11',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms/zeit.content.quiz',
