f5159bc3d11093d25e9628737c054091dd19bc96
Knut Goetz <Knut.Goetz@zeit.de>
2016-12-29 11:35:14 +0100

    Back to development: 0.5.13

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.quiz',
-    version='0.5.12',
+    version='0.5.13.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
